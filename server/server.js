0e4e2d4aef1dae93c28baab292bb86727282276a
ikhnaton <ikhnaton@techie.com>
2018-03-03 13:17:48 -0800

    layout most  of homepage

--- server/server.js
+++ server/server.js
@@ -1,14 +1,6 @@
 if (!process.env.VCAP_SERVICES)
 {
-	try
-	{
-		process.env.VCAP_SERVICES = JSON.stringify(require('../vcap.local'));
-	}
-	catch (error)
-	{
-		console.log(error);
-	}
-
+	process.env.VCAP_SERVICES = JSON.stringify(require('../vcap.local'));
 }
 
 //require('./server/db');
--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,7 @@ import { Provider, connect } from 'react-redux';
 import Messages from 'react-error';
 import * as messageActions from 'react-error/actions';
 import store from './store';
-import { Header, Card, Container } from 'semantic-ui-react';
+import { Header, Card, Divider, Menu, Grid, Image } from 'semantic-ui-react';
 import "./reset.less";
 import "./main.less";
 
@@ -19,11 +19,28 @@ class App extends React.Component
 	{
 		return (
 			<div>
-				<Header className="header" textAlign="center">
+				<Header className="mainheader" textAlign="center">
+					<Header.Subheader className="subheader">
+						<Menu className="menu">
+							<Menu.Item>About</Menu.Item>
+							<Menu.Item>Gallery</Menu.Item>
+							<Menu.Item>Pricing</Menu.Item>
+							<Menu.Item>Create NOW</Menu.Item>
+							<Menu.Item>Sign In</Menu.Item>
+						</Menu>
+					</Header.Subheader>
 					<Header.Content className="main" >
 						Your-Creations
+						<Divider horizontal={true} fitted={true} className="divider"/>
 					</Header.Content>
 				</Header>
+				<Grid>
+					<Grid.Row>
+						<Grid.Column>
+							<Image src="images/main.png" verticalALign="middle" centered={true}/>
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
 			</div>
 		);
 	}
